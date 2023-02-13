31a58bfe0ec1cf95c99f30fb7bc9d5938c8d06d9
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2023-02-13 04:40:16 -0300

    Add files via upload
    Hello world and server config

---
+++ Curso de PHP/01. Básico/000. Hello World/index.php
@@ -0,0 +1,30 @@
+<!-- 
+
+    echo or print => write on screen
+    \u{} => Add emoji 
+
+-->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP</title>
+</head>
+<body>
+    
+    <h1>
+        <?php 
+            echo "Hello, World! \u{1F30E}";
+            print " Print command is work too";
+        ?>
+    </h1>
+
+    <p>
+        First PHP to infinity and beyond.
+    </p>
+
+</body>
+</html>
